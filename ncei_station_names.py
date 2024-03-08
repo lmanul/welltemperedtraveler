2cab22a62ee8770f0a705184c8044ed7ad3db9f6
Manu Cornet <m@ma.nu>
2024-03-08 23:04:39 +0100

    Cairo, much better data

--- ncei_station_names.py
+++ ncei_station_names.py
@@ -1,6 +1,12 @@
 NAMES = {
+    "Bogotá|CO": "BOGOTA ELDORADO",
     "Brasília|BR": "BRASILIA",
+    # Might need to combine with CAIRO EZBEKIYA
+    "Cairo|EG": "CAIRO ABBASSIA",
     "Frankfurt|DE": "FRANKFURT MAIN STADT",
+    "Rome|IT": "ROMA CIAMPINO",
+    # Might be more than one exact match?
+    "São Paulo|BR": "SAO PAULO",
 }
 
 NCEI_COUNTRY_CODES = {
@@ -12,5 +18,7 @@ NCEI_COUNTRY_CODES = {
     "MN": "MG",
     "NE": "NG",
     "RU": "RS",
+    "SC": "SE",
+    "SN": "SG",
     "TD": "CD",
 }
