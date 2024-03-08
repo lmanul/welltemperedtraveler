f6eb4ee8cf6c3c4cb80aa6cb559ff384453bd029
Manu Cornet <m@ma.nu>
2024-03-08 22:57:01 +0100

    Zagreb, newer data

--- ncei_station_names.py
+++ ncei_station_names.py
@@ -1,11 +1,16 @@
 NAMES = {
     "Brasília|BR": "BRASILIA",
+    "Frankfurt|DE": "FRANKFURT MAIN STADT",
 }
 
 NCEI_COUNTRY_CODES = {
     "BY": "BO",
     "CN": "CH",
+    "DE": "GM",
     "KI": "KR",
+    "MG": "MA",
     "MN": "MG",
+    "NE": "NG",
     "RU": "RS",
+    "TD": "CD",
 }
