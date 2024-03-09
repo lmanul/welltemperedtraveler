cbf1455fcaf0268c9c156a65a871963e3deaff3b
Manu Cornet <m@ma.nu>
2024-03-09 15:57:23 +0100

    Little more refactoring

--- util.py
+++ util.py
@@ -9,3 +9,9 @@ def read_existing_data():
             (key, value) = line.split(":", 1)
             data[key] = json.loads(value)
     return data
+
+def read_cities():
+    with open("cities.txt", "r") as f:
+        lines = f.readlines()
+        return [l.strip() for l in lines]
+
