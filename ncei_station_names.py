88da3310c7d017785e61879c944bfedf83a8931f
Manu Cornet <m@ma.nu>
2024-03-09 15:36:13 +0100

    Bridgetwon data is oldish, but better than nothing

--- ncei_station_names.py
+++ ncei_station_names.py
@@ -1,11 +1,13 @@
 NAMES = {
     "Bogotá|CO": "BOGOTA ELDORADO",
     "Brasília|BR": "BRASILIA",
+    "Bridgetown|BB": "GRANTLEY ADAMS",
     # Might need to combine with CAIRO EZBEKIYA
     "Cairo|EG": "CAIRO ABBASSIA",
     "Frankfurt|DE": "FRANKFURT MAIN STADT",
+    "Kansas City|US": "KANSAS CITY BARRY HARBOR, M",
     "Los Angeles|US": "LOS ANGELES DOWNTOWN USC, C",
-    "Quebec City|CA": "QUEBEC, Q",
+    "Quebec City|CA": "QUEBEC 2, Q",
     "Pittsburgh|US": "PITTSBURGH INTERNATIONAL AIRPORT, P",
     "Rome|IT": "ROMA CIAMPINO",
     # Might be more than one exact match?
